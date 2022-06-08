ad5a6eb9696de7a613838cd986ed96503f0daac3
Bhavya Sharma <bhavyaas2662@hotmail.com>
2022-06-08 21:10:00 +0530

    adding comments

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
+#taking username as input from user
 username = input("Enter Username: ")
+#creating new socket for client
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(('127.0.0.1',9999))
 
@@ -8,6 +10,7 @@ def receive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
+            #sending username to server
             if message == 'USER':
                 client.send(username.encode('ascii'))
             else:
@@ -19,10 +22,13 @@ def receive():
 
 def write():
     while True:
+        #taking messages as input
         message= f'{username}: {input("")}'
         client.send(message.encode('ascii'))
+
+#starting receive thread to get messages from server
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
-
+#starting write thread to send messages to server
 write_thread = threading.Thread(target=write)
 write_thread.start()
--- server.py
+++ server.py
@@ -4,10 +4,13 @@ import socket
 #localhost
 host = '127.0.0.1'
 port = 9999
+#creating socket for server
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#binding server with our host and port
 server.bind((host,port))
+#starting server to listen
 server.listen()
-
+#creating lists for clients and usernames
 clients=[]
 usernames=[]
 
@@ -19,9 +22,12 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
+            #recveing message from client
             message=client.recv(1024)
+            #broadcasting it to every client
             broadcast(message)
         except:
+            #if client is not responding, remove that client
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -32,19 +38,22 @@ def handle(client):
 
 def receive():
     while True:
+
         client, address = server.accept()
         print('Connected with ',address)
-
+        #asking client for username
         client.send('USER'.encode('ascii'))
+        #getting username from client
         username = client.recv(1024).decode('ascii')
+        #adding username and new client to list
         usernames.append(username)
         clients.append(client)
 
         print(f'Username of the client is {username}')
         broadcast(f'{username} joined the chat'.encode('ascii'))
         client.send("Connected to the server".encode('ascii'))
-
+        #starting a new thread for handling client messages
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-print("Server is listening")
+print("Server is on")
 receive()
